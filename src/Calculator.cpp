df652c14f048d4063de9f3ed24402bf5d317b463
Cody G Peterson <peterson.cody16@gmail.com>
2022-09-01 15:34:52 -0600

    Declared and defined 'Evaluate' to allow the calculator to decide which function it should use.

--- src/Calculator.cpp
+++ src/Calculator.cpp
@@ -8,6 +8,37 @@
 
 #include "Calculator.h"
 
+// Evaluate method, decides which operation to perform
+void Evaluate(char* expression)
+{
+	// Declare variables
+	double a, b;
+	char op;
+
+	// Scan expression for values
+	sscanf(expression, "%lf %c %lf", &a, &op, &b);
+
+	// Switch statement to decide which operation to perform
+	switch (op)
+	{
+	case '+':
+		std::cout << Add(a, b) << std::endl;
+		break;
+	case '-':
+		std::cout << Subtract(a, b) << std::endl;
+		break;
+	case '*':
+		std::cout << Multiply(a, b) << std::endl;
+		break;
+	case '/':
+		std::cout << Divide(a, b) << std::endl;
+		break;
+	default:
+		std::cout << "Invalid operator" << std::endl;
+		break;
+	}
+}
+
 // Add method, returns sum of two numbers as double
 double Add(double a, double b)
 {
